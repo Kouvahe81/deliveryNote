ca708ca1fc632ead71f7aef194f866819d1b3e46
Kouvahe81 <93136077+Kouvahe81@users.noreply.github.com>
2024-07-03 05:37:13 +0200

    ajout des fpages utilisateurs et fonctions et quelques corrections

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Auth0Provider } from '@auth0/auth0-react';
 import Home from './pages/home';
+import Error from './components/error' 
+import AutoLogout from './components/autoLogout';
+import CreatePersonFunction from './pages/createPersonFunction';
+import PersonFunction from './pages/personFunction';
+import Person from './pages/person';
+import CreatePerson from './pages/createPerson';
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
@@ -23,6 +29,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           {/* Route par défaut */}
           <Route path="*" element={<Home/>} />
           <Route path="/home" element={<Home/>} />
+          <Route path="/error" element={<Error/>}/>
+          <Route path='/createPersonFunction' element={<CreatePersonFunction/>}/>   
+          <Route path='/personFunction' element={<PersonFunction/>}/>
+          <Route path='/createPerson' element={<CreatePerson/>}/>
+          <Route path="/person" element={<Person/>} />
         </Routes>
       </BrowserRouter>
     </Auth0Provider>
