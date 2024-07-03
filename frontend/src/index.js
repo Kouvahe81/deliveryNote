3c42b594ae175b3cd253fc844749541b5415837b
Kouvahe81 <93136077+Kouvahe81@users.noreply.github.com>
2024-07-03 05:42:00 +0200

    correction dans index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -23,7 +23,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         prompt: 'login',
       }}
     >
-      
+      <AutoLogout /> {/* Charge le composant AutoLogout*/}
       <BrowserRouter>
         <Routes>
           {/* Route par défaut */}
