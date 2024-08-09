fcdf6857569a3fcf4abee6abdf2630495747b8fd
Kouvahe81 <93136077+Kouvahe81@users.noreply.github.com>
2024-08-10 00:25:36 +0200

    Update listDeliveryNote.js

--- frontend/src/pages/listDeliveryNote.js
+++ frontend/src/pages/listDeliveryNote.js
@@ -115,7 +115,9 @@ const ListDeliveryNote = () => {
         const noteToEdit = deliveryNote.find(note => note.deliveryNoteId === deliveryNoteId);
         if (noteToEdit) {
             // Redirection vers le formulaire de modification avec les détails du bon de livraison
-            window.location.href = `/finalDeliveryNote?deliveryNoteId=${deliveryNoteId}`;
+            //window.location.href = `/finalDeliveryNote?deliveryNoteId=${deliveryNoteId}`;
+            window.location.href = `${window.location.origin}/finalDeliveryNote?deliveryNoteId=${deliveryNoteId}`;
+
         }
     };
 
